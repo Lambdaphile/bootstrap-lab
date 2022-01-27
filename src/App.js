9c181f0edc70b69f854c2d2f5d81353e666bd35b
λ • genesis <rwxgenesis@gmail.com>
2022-01-27 16:08:25 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-primary">Hello Bootstrap</button>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
